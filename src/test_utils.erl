594ccab3f04e1f711a63ef7ab2669221f7ae9fde
Kuba Odias <kuba.odias@relayr.io>
2018-02-09 07:29:45 +0100

    Added meck_assert_not_called/3

--- src/test_utils.erl
+++ src/test_utils.erl
@@ -29,8 +29,8 @@
 	meck_call_args/2,
 	meck_last_call_args/2,
 	meck_assert_called_once/3,
-	meck_assert_not_called/3,
 	meck_assert_not_called/2,
+	meck_assert_not_called/3,
 	meck_num_calls/2,
 	shuffle/1,
     list_containing/1
@@ -282,19 +282,21 @@ meck_last_call_args(Module, Function) ->
 		{ok, [LastCallArgs]}
 	end.
 
--spec meck_assert_called_once(Module :: atom(), Function :: atom(), Args :: list()) -> ok.
+-spec meck_assert_called_once(Module :: atom(), Function :: atom(), Args :: list() | '_') -> ok.
 meck_assert_called_once(Module, Function, Args) ->
-	?assertEqual(1, meck:num_calls(Module, Function, Args)),
-	ok.
-
--spec meck_assert_not_called(Module :: atom(), Function :: atom(), Args :: list()) -> ok.
-meck_assert_not_called(Module, Function, Args) ->
-	?assertEqual(0, meck:num_calls(Module, Function, Args)),
-	ok.
+	meck_assert_num_calls(1, Module, Function, Args).
 
 -spec meck_assert_not_called(Module :: atom(), Function :: atom()) -> ok.
 meck_assert_not_called(Module, Function) ->
-	?assertEqual(0, meck_num_calls(Module, Function)),
+	meck_assert_not_called(Module, Function, '_').
+
+-spec meck_assert_not_called(Module :: atom(), Function :: atom(), Args :: list() | '_') -> ok.
+meck_assert_not_called(Module, Function, Args) ->
+	meck_assert_num_calls(0, Module, Function, Args).
+
+-spec meck_assert_num_calls(NumCalls :: non_neg_integer(), Module :: atom(), Function :: atom(), Args :: list() | '_') -> ok.
+meck_assert_num_calls(NumCalls, Module, Function, Args) ->
+	?assertEqual(NumCalls, meck:num_calls(Module, Function, Args)),
 	ok.
 
 shuffle([])     -> [];
