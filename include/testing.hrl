6ac10d973d5a05a5d029409f5f9bfdd048fe902c
Kuba Odias <kuba.odias@relayr.io>
2019-07-18 15:04:52 +0200

    Changed tabs to 4 spaces

--- include/testing.hrl
+++ include/testing.hrl
@@ -17,11 +17,11 @@
 -define(PROP_TEST_FUN(), -prop_test_function([])).
 -define(TEST_CASE(FunName), {atom_to_list(FunName), fun FunName/0}).
 -define(LOOPER(Fun, Args, LoopTimeout, Count), 
-		test_utils:state_sleep_looper(Fun, Args, LoopTimeout, Count)).
+        test_utils:state_sleep_looper(Fun, Args, LoopTimeout, Count)).
 -define(WAIT_FOR_PROCESS_STOPPED(ProcessRegisteredNameOrID),
-		test_utils:wait_for_process_stopped(ProcessRegisteredNameOrID)).
+        test_utils:wait_for_process_stopped(ProcessRegisteredNameOrID)).
 -define(RECOMPILE(Module, FunctionDefs),
-		test_utils:recompile_module(Module, FunctionDefs)).
+        test_utils:recompile_module(Module, FunctionDefs)).
 -define(MECK(Module, Funs), test_utils:meck_module(Module, Funs)).
 -define(MECK_LOOP(Module, Funs), test_utils:meck_loop_module(Module, Funs)).
 -define(MECK_RESET(Module), meck:reset(Module)).
@@ -44,99 +44,99 @@
 -define(EQC_USHORT_GEN, choose(0,65535)).
 -define(EQC_ULONG_GEN, choose(0,18446744073709551615)).
 -define(EQC_BIN_GEN(MaxSize),
-		?LET(Size,
-			 choose(0, MaxSize),
-			 ?LET(List,
-				  vector(Size, ?EQC_BYTE_GEN),
-				  list_to_binary(List)))).
+        ?LET(Size,
+             choose(0, MaxSize),
+             ?LET(List,
+                  vector(Size, ?EQC_BYTE_GEN),
+                  list_to_binary(List)))).
 
 -define(assertContains(Element, List), begin ((
-	fun(E, L) ->
-		case lists:member(E, L) of
-			true -> ok;
-			__V -> erlang:error({assert,
-				[{module, ?MODULE},
-					{line, ?LINE},
-					{expected, L ++ [E]},
-					{value, L}]})
-		end
-	end
+    fun(E, L) ->
+        case lists:member(E, L) of
+            true -> ok;
+            __V -> erlang:error({assert,
+                [{module, ?MODULE},
+                    {line, ?LINE},
+                    {expected, L ++ [E]},
+                    {value, L}]})
+        end
+    end
 )(Element, List))end).
 
 -define(assertContainsAll(Elements, List), begin ((
-	fun(Es, L) ->
-		case lists:all(fun(E) -> lists:member(E, L) end, Es) of
-			true -> ok;
-			__V -> erlang:error({assert,
-				[{module, ?MODULE},
-					{line, ?LINE},
-					{expected, L ++ Es},
-					{value, L}]})
-		end
-	end
+    fun(Es, L) ->
+        case lists:all(fun(E) -> lists:member(E, L) end, Es) of
+            true -> ok;
+            __V -> erlang:error({assert,
+                [{module, ?MODULE},
+                    {line, ?LINE},
+                    {expected, L ++ Es},
+                    {value, L}]})
+        end
+    end
 )(Elements, List))end).
 
 -define(assertNotContains(Element, List), begin ((
-		fun(E, L) ->
-			case lists:member(E, L) of
-				false -> ok;
-				__V -> erlang:error({assert,
-					[{module, ?MODULE},
-						{line, ?LINE},
-						{expected, L -- [E]},
-						{value, L}]})
-			end
-		end
+        fun(E, L) ->
+            case lists:member(E, L) of
+                false -> ok;
+                __V -> erlang:error({assert,
+                    [{module, ?MODULE},
+                        {line, ?LINE},
+                        {expected, L -- [E]},
+                        {value, L}]})
+            end
+        end
 )(Element, List))end).
 
 
 -define(SORT_PROPERTIES_IN_JSON(U), begin((
-	%For me is tricky - i leaved commented lines for better understanding and code review.
-		fun
-			 SSort([FirstTuple | RestOfTuples]) when is_tuple(FirstTuple) ->
-				 %?debugFmt("3 List of many tuples: ~p", [[FirstTuple, RestOfTuples]]),
-				 R = ?SORT(lists:merge([SSort(FirstTuple)], SSort(RestOfTuples))),
-				 %?debugFmt("3 R: ~p", [R]),
-				 R;
+    %For me is tricky - i leaved commented lines for better understanding and code review.
+        fun
+             SSort([FirstTuple | RestOfTuples]) when is_tuple(FirstTuple) ->
+                 %?debugFmt("3 List of many tuples: ~p", [[FirstTuple, RestOfTuples]]),
+                 R = ?SORT(lists:merge([SSort(FirstTuple)], SSort(RestOfTuples))),
+                 %?debugFmt("3 R: ~p", [R]),
+                 R;
 
-			 SSort(ArrayOfElements) when is_list(ArrayOfElements) ->
-				 %?debugFmt("4 List of many elements: ~p", [ArrayOfElements]),
-				 R = [SSort(E) || E <- ArrayOfElements],
-				 %?debugFmt("4 R: ~p", [R]),
-				 R;
+             SSort(ArrayOfElements) when is_list(ArrayOfElements) ->
+                 %?debugFmt("4 List of many elements: ~p", [ArrayOfElements]),
+                 R = [SSort(E) || E <- ArrayOfElements],
+                 %?debugFmt("4 R: ~p", [R]),
+                 R;
 
-			 SSort({AttrName, AttrValue}) when is_atom(AttrName) ->
-				 SSort({atom_to_binary(AttrName, utf8), AttrValue});
+             SSort({AttrName, AttrValue}) when is_atom(AttrName) ->
+                 SSort({atom_to_binary(AttrName, utf8), AttrValue});
 
-			 SSort({AttrName, AttrValue}) when is_integer(AttrName) ->
-				 SSort({integer_to_binary(AttrName), AttrValue});
+             SSort({AttrName, AttrValue}) when is_integer(AttrName) ->
+                 SSort({integer_to_binary(AttrName), AttrValue});
 
-			 SSort({AttrName, AttrValue}) ->
-				 %?debugFmt("5 Tuple: ~p", [{AttrName, AttrValue}]),
-				 R = {AttrName, SSort(AttrValue)},
-				 %?debugFmt("5 R: ~p", [R]),
-				 R;
+             SSort({AttrName, AttrValue}) ->
+                 %?debugFmt("5 Tuple: ~p", [{AttrName, AttrValue}]),
+                 R = {AttrName, SSort(AttrValue)},
+                 %?debugFmt("5 R: ~p", [R]),
+                 R;
 
-			 SSort(Primitive) ->
-				 %?debugFmt("Primitive: ~p", [Primitive]),
-				 Primitive
-		 end
+             SSort(Primitive) ->
+                 %?debugFmt("Primitive: ~p", [Primitive]),
+                 Primitive
+         end
 ))(U)end).
 
 -define(assertJson(Expected, Actual), begin ((
-	fun(E, A) ->
-		StripWhitespaces = fun
-			  (Binary) when is_binary(Binary) ->
-				  jsx:encode(?SORT_PROPERTIES_IN_JSON(jsx:decode(Binary)));
-			  (JSX) ->
-				  jsx:encode(?SORT_PROPERTIES_IN_JSON(JSX))
-		  end,
-		StripedE = StripWhitespaces(E),
-		StripedA = StripWhitespaces(A),
-		io:format("~nExpected: ~p", [StripedE]),
-		io:format("~nActual  : ~p", [StripedA]),
-		?assertEqual(StripedE, StripedA)
-	end
+    fun(E, A) ->
+        StripWhitespaces = fun
+              (Binary) when is_binary(Binary) ->
+                  jsx:encode(?SORT_PROPERTIES_IN_JSON(jsx:decode(Binary)));
+              (JSX) ->
+                  jsx:encode(?SORT_PROPERTIES_IN_JSON(JSX))
+          end,
+        StripedE = StripWhitespaces(E),
+        StripedA = StripWhitespaces(A),
+        io:format("~nExpected: ~p", [StripedE]),
+        io:format("~nActual  : ~p", [StripedA]),
+        ?assertEqual(StripedE, StripedA)
+    end
 )(Expected, Actual))end).
 
 -define(assertEqualUnordered(Expected, Actual), ?assertEqual(?SORT(Expected), ?SORT(Actual))).
@@ -170,11 +170,11 @@
                             {Module, Function, Args}
                     end,
                 ArgsStr =
-					if Args =:= '_' ->
-						"...";
-					true ->
-						string:join([io_lib:format("~p", [Arg]) || Arg <- Args], ",")
-					end,
+                    if Args =:= '_' ->
+                        "...";
+                    true ->
+                        string:join([io_lib:format("~p", [Arg]) || Arg <- Args], ",")
+                    end,
                 erlang:error({assert, [
                     {module, ?MODULE},
                     {line, ?LINE},
