ca054ac7093876184a6d7c5ff05470bfc4673322
Kuba Odias <kuba.odias@relayr.io>
2019-08-28 05:46:40 +0200

    Added separate include file and test module for property-based testing. Changed string generator to include all ASCII.

--- include/testing.hrl
+++ include/testing.hrl
@@ -4,17 +4,15 @@
 
 -define(testing_hrl, true).
 
--include_lib("proper/include/proper.hrl").
+-include("prop_testing.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
 -import(test_utils, [shuffle/1]).
 
 -compile(export_all).
 -compile({parse_transform, test_parse_transform}).
--compile({parse_transform, prop_test_parse_transform}).
 
 -define(TEST_FUN(), -test_function([])).
--define(PROP_TEST_FUN(), -prop_test_function([])).
 -define(TEST_CASE(FunName), {atom_to_list(FunName), fun FunName/0}).
 -define(LOOPER(Fun, Args, LoopTimeout, Count), 
         test_utils:state_sleep_looper(Fun, Args, LoopTimeout, Count)).
@@ -32,23 +30,6 @@
 
 -define(IDENTITY, fun(X) -> X end).
 -define(IDENTITY_OK, fun(X) -> {ok, X} end).
-
-% QuickCheck generators
--define(EQC_STRING_GEN, non_empty(list(choose($a,$z)))).
--define(EQC_ATOM_GEN, ?LET(Name, ?EQC_STRING_GEN, list_to_atom(Name))).
--define(EQC_LIST_GEN, ?LET(Name, non_empty(list(choose(0,255))), Name)).
--define(EQC_SORTED_UNIQUE_LIST_GEN(Gen), ?LET(Values2, list(Gen), lists:usort(Values2))).
--define(EQC_UNIQUE_LIST_GEN(Gen), ?LET(List, ?EQC_SORTED_UNIQUE_LIST_GEN(Gen), shuffle(List))).
--define(EQC_NESTED_LIST_GEN, ?LET(List, ?EQC_LIST_GEN, List)).
--define(EQC_BYTE_GEN, choose(0,255)).
--define(EQC_USHORT_GEN, choose(0,65535)).
--define(EQC_ULONG_GEN, choose(0,18446744073709551615)).
--define(EQC_BIN_GEN(MaxSize),
-        ?LET(Size,
-             choose(0, MaxSize),
-             ?LET(List,
-                  vector(Size, ?EQC_BYTE_GEN),
-                  list_to_binary(List)))).
 
 -define(assertContains(Element, List), begin ((
     fun(E, L) ->
